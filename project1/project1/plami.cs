1459c42024d215a1f3b3f4456c198a656b86ba5b
plamenaaleksieva <plami_2006@abv.bg>
2023-02-05 13:33:31 +0200

    concat

---
+++ project1/project1/plami.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    class plami
+    {
+        static string Concat(string a, string b)
+        {
+            string a1 = "a";
+            string b1 = "b";
+            return a + b;
+        }
+
+    }
+}
