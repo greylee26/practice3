5a1a106d45bae9148d954b7ee9d3a041e0ef8e9c
Tanya Grudeva <63243221+tanita22@users.noreply.github.com>
2023-02-05 13:42:31 +0200

    sum

---
+++ project1/project1/nikola.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    internal class nikola
+    {
+        static double Sum(double a, double b)
+        {
+            double a1 = 4;
+            double b1 = 4.5;
+            return a + b;
+        }
+    }
+}
---
+++ sum/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 3;
+int b = 4;
+int sum = a + b;
